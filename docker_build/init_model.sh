0d2ebc3ee0d12802c2ab79db1096ed731d130145
sifat95 <sifathasnain95@gmail.com>
2022-03-24 12:03:52 +0600

    fix: model-archive command run on windows

--- docker_build/init_model.sh
+++ docker_build/init_model.sh
@@ -1,4 +1,5 @@
 
+#! /bin/bash
 torch-model-archiver --model-name dr-score \
 --version 1.0 --model-file /home/model-server/model.py \
 --serialized-file best.pth \
@@ -9,19 +10,4 @@ torch-model-archiver --model-name dr-score \
 ls *.mar
 
 mkdir model_store
-mv dr-score.mar model_store/
-
-#response=$(curl --write-out %{http_code} --silent --output /dev/null --retry 5 -X POST "http://localhost:8081/models?url=https://torchserve.s3.amazonaws.com/mar_files/resnet-18.mar&initial_workers=1&synchronous=true")
-#
-#if [ ! "$response" == 200 ]
-#then
-#    echo "failed to register model with torchserve"
-#else
-#    echo "successfully registered resnet-18 model with torchserve"
-#fi
-#
-#echo "TorchServe is up and running with resnet-18 model"
-#echo "Management APIs are accessible on http://127.0.0.1:8081"
-#echo "Inference APIs are accessible on http://127.0.0.1:8080"
-#echo "For more details refer TorchServe documentation"
-#echo "To stop docker container for TorchServe use command : docker container stop $container_id"
+mv dr-score.mar model_store
